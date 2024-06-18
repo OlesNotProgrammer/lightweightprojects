ecd4f9a85bca7f8a71a4737d8b926101ed3eba03
Oles <alexkiyhop@gmail.com>
2024-06-18 23:43:11 +0300

    blackout 18.06.24 23:43

--- src/main/java/com/example/lightweightprojects/FileReader.java
+++ src/main/java/com/example/lightweightprojects/FileReader.java
@@ -2,45 +2,26 @@ package com.example.lightweightprojects;
 
 import static java.util.stream.Collectors.joining;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Objects;
 import java.util.stream.Stream;
 
 public class FileReader {
     public static String readWholeFile(String fileName) {
-        Path filePath;
+        String result = null;
+        Stream<String> fileStream = null;
 
         try {
-            filePath = createPathFromFileName(fileName);
-        } catch (URISyntaxException e) {
+            fileStream = Files.lines(Paths.get(fileName));
+            result = fileStream.collect(joining("\n"));
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (fileStream != null) {
+                fileStream.close();
+            }
         }
 
-        try (Stream<String> fileLinesStream = openFileLinesStream(filePath)) {
-            return fileLinesStream.collect(joining("\n"));
-        }
-    }
-
-    private static Path createPathFromFileName(String fileName) throws URISyntaxException {
-        Objects.requireNonNull(fileName);
-        URL fileUrl = FileReader.class.getClassLoader().getResource(fileName);
-        try {
-            return Paths.get(fileUrl.toURI());
-        } catch (URISyntaxException e) {
-            throw e;
-        }
-    }
-
-    private static Stream<String> openFileLinesStream(Path filePath) throws IOException {
-        try {
-            return Files.lines(filePath);
-        } catch (IOException e) {
-            throw e;
-        }
+        return result;
     }
 }
